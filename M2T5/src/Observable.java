4e7569e93b62ceb553f6a2338a9b0d3f14b11e96
jeffson66 <linj3@rose-hulman.edu>
2021-01-28 11:49:15 -0500

    add order

---
+++ M2T5/src/Observable.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public abstract class Observable {
+	ArrayList<Observer> observers = new ArrayList<Observer>();
+	
+//	public void add();
+//	public void remove();
+	
+}
--- M2T5/src/Order.java
+++ M2T5/src/Order.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 import javax.swing.text.html.Option;
 
 public class Order {
-	int orderID;
+	String orderID;
 	String street;
-	int zip;
+	String zip;
 	String drink;
 	ArrayList<Condiment> condiments;
 	
-	public Order(int orderID, String street, int zip, String drink) {
+	public Order(String orderID, String street, String zip, String drink) {
 		this.orderID = orderID;
 		this.street = street;
 		this.zip = zip;
@@ -17,7 +17,7 @@ public class Order {
 		this.condiments = new ArrayList<>();
 	}
 	
-	public Order(int orderID, String streetAddress, int zipcode, String drink, ArrayList<Condiment> condiments) {
+	public Order(String orderID, String streetAddress, String zipcode, String drink, ArrayList<Condiment> condiments) {
 		this.orderID = orderID;
 		this.street = streetAddress;
 		this.zip = zipcode;
@@ -25,7 +25,7 @@ public class Order {
 		this.condiments = condiments;
 	}
 	
-	public int getOrderID() { 
+	public String getOrderID() { 
 		return this.orderID;
 	}
 	
@@ -33,7 +33,7 @@ public class Order {
 		return this.street;
 	}
 	
-	public int getZipcode() {
+	public String getZipcode() {
 		return this.zip;
 	}
 	
